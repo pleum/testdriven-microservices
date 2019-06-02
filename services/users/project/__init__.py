c93f3d317587f8a3519ad55be9c083711d001633
Chacree <chacree.som@cpc.ac.th>
2019-06-02 23:18:07 +0700

    Added users database setup

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,5 +1,9 @@
+import os
+import sys
+
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
@@ -7,7 +11,25 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    """User model"""
+    __table__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
 
 class UserPing(Resource):
     def get(self):
@@ -16,4 +38,7 @@ class UserPing(Resource):
             'message': 'pong!'
         }
 
+
 api.add_resource(UserPing, '/users/ping')
+
+# print(app.config, file=sys.stderr)
